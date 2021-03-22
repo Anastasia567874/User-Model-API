60a8b1b150ecd300098d62c5c83b2e0251d7d637
Admin <anastasia.vecherenko@yandex.ru>
2021-03-22 17:13:15 +0500

    Первый коммит

---
+++ main.py
@@ -0,0 +1,99 @@
+from flask import Flask, redirect, render_template
+from flask_login import LoginManager, login_user, login_required, logout_user
+from data.users import User
+from data.db_session import global_init, create_session
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+from forms.user import RegisterForm
+from data.jobs import Jobs
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = create_session()
+    return db_sess.query(User).get(user_id)
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/')
+def table():
+    global_init("db/mars_explorer.db")
+    db_sess = create_session()
+    list_jobs = []
+    for job in db_sess.query(Jobs).all():
+        list_jobs.append({'id': job.id, 'title': job.job, 'leader': f'{job.user.name} {job.user.surname}',
+                          'duration': job.work_size, 'list': job.collaborators, 'finish': job.is_finished})
+    return render_template('table.html', data=list_jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        global_init('db/mars_explorer.db')
+        db_sess = create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            email=form.email.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        global_init("db/mars_explorer.db")
+        db_sess = create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+
